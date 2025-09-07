e90b656d3153ece2724fa69240a524a52ff54103
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-07 20:23:16 +0545

    chore: update counter app

--- 01-mini-react-proj/src/App.jsx
+++ 01-mini-react-proj/src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
-import randomLogo from '../public/random-svgrepo-com.svg'
-import './App.css'
+// import randomLogo from '../public/random-svgrepo-com.svg'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function MyButton({ info, onClick }) {
   return (
-    <>
-      <div className='py-4 flex justify-end  items-center'>
-          <img src={randomLogo} className="logo random w-14 mr-auto" alt="random logo" />
-          <button className='hover:bg-red-300 border-1 px-2 rounded-md cursor-pointer' onClick={() => setCount(0)}>Reset</button>
-      </div>
-      <div className="count-log text-9xl py-24">
-      {count}
-      </div>
-      <button className='hover:bg-red-300 border-1 px-2 rounded-md cursor-pointer' onClick={() => setCount(count + 1)}>Increment</button><br /><br />
-      <button className='hover:bg-red-300 border-1 px-2 rounded-md cursor-pointer' onClick={() => setCount(count-1)}>Decrement</button>
-    </>
-  )
+  <button onClick={onClick} className="border-1 cursor-pointer px-2 rounded-md my-12">{info.name}</button>
+  );
+
 }
 
-export default App
+export default function App() {
+  let [count, setCount] = useState(0);
+  return (
+    <>
+    <MyButton onClick={() => setCount(0)} info={{name: "Reset"}} />
+      <div className="text-9xl p-12">{count}</div>
+      <div className="flex gap-36 justify-center items-center">
+      <MyButton onClick={() => setCount(count + 1)} info={{name: 'Increase'}} />
+      <MyButton onClick={() => setCount(count - 1)} info={{name: 'Decrease'}} />
+      </div>
+    </>
+  );
+}
