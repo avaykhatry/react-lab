d796c861dd6e655d9222d11eec2df41995fd0948
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-11 22:01:18 +0545

    feat: add last count value to storage

--- 01-mini-react-proj/src/App.jsx
+++ 01-mini-react-proj/src/App.jsx
@@ -1,24 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import reactLogo from './assets/react.svg'
 // import randomLogo from '../public/random-svgrepo-com.svg'
 import "./App.css";
 
 function MyButton({ info, onClick }) {
   return (
-  <button onClick={onClick} className="border-1 cursor-pointer px-2 rounded-md my-12">{info.name}</button>
+    <button
+      onClick={onClick}
+      className="border-1 cursor-pointer px-2 rounded-md my-12"
+    >
+      {info.name}
+    </button>
   );
-
 }
 
+  function useLocalStorage(key, firstValue) {
+    const [value, setValue] = useState(() => {
+      const stored = localStorage.getItem(key);
+      return stored ? JSON.parse(stored) : firstValue;
+    });
+
+    useEffect(() => {
+      localStorage.setItem('count', JSON.stringify(value));
+    }, [key, value]);
+
+    return [value, setValue];
+  }
+
 export default function App() {
-  let [count, setCount] = useState(0);
+  const [count, setCount] = useLocalStorage('count', 0);
+
   return (
     <>
-    <MyButton onClick={() => setCount(0)} info={{name: "Reset"}} />
+      <MyButton onClick={() => setCount(0)} info={{ name: "Reset" }} />
       <div className="text-9xl p-12">{count}</div>
       <div className="flex gap-36 justify-center items-center">
-      <MyButton onClick={() => setCount(count + 1)} info={{name: 'Increase'}} />
-      <MyButton onClick={() => setCount(count - 1)} info={{name: 'Decrease'}} />
+        <MyButton
+          onClick={() => setCount(count + 1)}
+          info={{ name: "Increase" }}
+        />
+        <MyButton
+          onClick={() => setCount(count - 1)}
+          info={{ name: "Decrease" }}
+        />
       </div>
     </>
   );
