7180e2e5b81f4d6a6b1087c3708f3f9244ce3a9a
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-12 19:35:29 +0545

    chore: add local storage to save the tasks

--- 01-mini-react-proj/src/ToDoList.jsx
+++ 01-mini-react-proj/src/ToDoList.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 let nextId = 0;
 
+function useLocalStorage(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    const stored = localStorage.getItem(key);
+    return stored ? JSON.parse(stored) : initialValue;
+  })
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
 export default function ToDoList() {
     const [task, setTask] = useState("");
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useLocalStorage('tasks', []);
 
   return (
     <>
