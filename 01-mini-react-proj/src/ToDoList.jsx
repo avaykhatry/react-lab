94060a7b00e4c1ba56dd1d39519deaa7507ecaad
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-12 22:18:01 +0545

    chore: add index value to Storage

--- 01-mini-react-proj/src/ToDoList.jsx
+++ 01-mini-react-proj/src/ToDoList.jsx
@@ -1,54 +1,78 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-let nextId = 0;
 
+// localStorage Component
+// start
 function useLocalStorage(key, initialValue) {
   const [value, setValue] = useState(() => {
     const stored = localStorage.getItem(key);
     return stored ? JSON.parse(stored) : initialValue;
-  })
+  });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value))
+    localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
   return [value, setValue];
 }
 
+// end
+
 export default function ToDoList() {
-    const [task, setTask] = useState("");
-    const [tasks, setTasks] = useLocalStorage('tasks', []);
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useLocalStorage("tasks", []);
+  const [nextId, setNextId] = useLocalStorage('nextId', 0);
+
+  function handleChange(e) {
+    setTask(e.target.value);
+  }
+
+  function addItem() {
+      setTasks([...tasks, { id: nextId, task }]); // here, task is shorthand for task:task, since for the matching key and variable name, we can use shorthand
+      setNextId(nextId + 1);
+  }
+  
+  function handleBtnClick() {
+    addItem();
+    setTask("");
+  }
 
   return (
     <>
       <h1 className="text-6xl underline py-6 text-center">Task Tracker</h1>
       <div className="tasks">
-        <ul className='p-4'>
-            {tasks.map((t) => (
-                <div className='flex gap-8 justify-center items-center'>
-                    <input className='block' type="checkbox" />
-                    <li key={t.id}>{t.task}</li>
-                    <button className='cursor-pointer' onClick={() => {
-                        setTasks(
-                            tasks.filter(tsk => tsk.id !== t.id)
-                        );
-                    }}><img src="src/assets/remove-minus.svg" width="28px" alt="" /></button>
-                </div>
-            )) 
-            }
+        <ul className="p-4">
+          {tasks.map((t) => (
+            <li key={t.id} className="flex gap-8 justify-center items-center">
+              <input type="checkbox" />
+             {t.task}
+              <button
+                className="cursor-pointer"
+                onClick={() => {
+                  setTasks(tasks.filter((tsk) => tsk.id !== t.id));
+                }}
+              >
+                <img src="src/assets/remove-minus.svg" width="28px" alt="" />
+              </button>
+            </li>
+          ))}
         </ul>
         <ul>
           <li>
             <input
-            onChange={e => setTask(e.target.value)}
+              onChange={handleChange}
               className="border px-2 rounded-l-xl"
               type="text"
-              placeholder="Add a new task" autoFocus
+              placeholder="Add a new task"
+              autoFocus={true}
               value={task}
             />
-            <button onClick={() => {
-                setTasks([...tasks, { id: nextId++, task: task}])
-            }} className='cursor-pointer border px-1 rounded-r-xl'>Add Task</button>
+            <button
+              onClick={handleBtnClick}
+              className="cursor-pointer border px-1 rounded-r-xl" disabled={task.length === 0}
+            >
+              Add Task
+            </button>
           </li>
         </ul>
       </div>
