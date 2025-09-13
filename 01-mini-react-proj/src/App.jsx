021ab2e99360b2cb3c7ebdc2f13c63bcad6dc031
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-13 20:59:18 +0545

    feat: Add a page that contains all the other mini apps

--- 01-mini-react-proj/src/App.jsx
+++ 01-mini-react-proj/src/App.jsx
@@ -1,48 +1,17 @@
-import { useState, useEffect } from "react";
-// import reactLogo from './assets/react.svg'
-// import randomLogo from '../public/random-svgrepo-com.svg'
-import "./App.css";
-
-function MyButton({ info, onClick }) {
+function Button({ children }) {
   return (
-    <button
-      onClick={onClick}
-      className="border-1 cursor-pointer px-2 rounded-md my-12"
-    >
-      {info.name}
-    </button>
+    <div>
+      <button className="border px-2 rounded-xl">{children}</button>
+    </div>
   );
 }
 
-  function useLocalStorage(key, firstValue) {
-    const [value, setValue] = useState(() => {
-      const stored = localStorage.getItem(key);
-      return stored ? JSON.parse(stored) : firstValue;
-    });
-
-    useEffect(() => {
-      localStorage.setItem('count', JSON.stringify(value));
-    }, [key, value]);
-
-    return [value, setValue];
-  }
-
 export default function App() {
-  const [count, setCount] = useLocalStorage('count', 0);
-
   return (
     <>
-      <MyButton onClick={() => setCount(0)} info={{ name: "Reset" }} />
-      <div className="text-9xl p-12">{count}</div>
-      <div className="flex gap-36 justify-center items-center">
-        <MyButton
-          onClick={() => setCount(count + 1)}
-          info={{ name: "Increase" }}
-        />
-        <MyButton
-          onClick={() => setCount(count - 1)}
-          info={{ name: "Decrease" }}
-        />
+      <div className="container p-8 flex flex-col gap-8">
+        <Button>Click The Count</Button>
+        <Button>Track The Tasks</Button>
       </div>
     </>
   );
---
+++ 01-mini-react-proj/src/Counter.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+// import reactLogo from './assets/react.svg'
+// import randomLogo from '../public/random-svgrepo-com.svg'
+import "./App.css";
+
+function MyButton({ info, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className="border-1 cursor-pointer px-2 rounded-md my-12"
+    >
+      {info.name}
+    </button>
+  );
+}
+
+  function useLocalStorage(key, firstValue) {
+    const [value, setValue] = useState(() => {
+      const stored = localStorage.getItem(key);
+      return stored ? JSON.parse(stored) : firstValue;
+    });
+
+    useEffect(() => {
+      localStorage.setItem('count', JSON.stringify(value));
+    }, [key, value]);
+
+    return [value, setValue];
+  }
+
+export default function Counter() {
+  const [count, setCount] = useLocalStorage('count', 0);
+
+  return (
+    <>
+      <MyButton onClick={() => setCount(0)} info={{ name: "Reset" }} />
+      <div className="text-9xl p-12">{count}</div>
+      <div className="flex gap-36 justify-center items-center">
+        <MyButton
+          onClick={() => setCount(count + 1)}
+          info={{ name: "Increase" }}
+        />
+        <MyButton
+          onClick={() => setCount(count - 1)}
+          info={{ name: "Decrease" }}
+        />
+      </div>
+    </>
+  );
+}
