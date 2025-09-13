b471293942a4f300959b3bdc0469ae91ca518730
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-13 22:13:02 +0545

    chore: toggle application and back button in the home page

--- 01-mini-react-proj/src/App.jsx
+++ 01-mini-react-proj/src/App.jsx
@@ -1,18 +1,35 @@
-function Button({ children }) {
+import { useState } from "react";
+import Counter from "./Counter";
+import ToDoList from "./ToDoList";
+
+function Button({ onClick, children }) {
   return (
     <div>
-      <button className="border px-2 rounded-xl">{children}</button>
+      <button onClick={onClick} className={` border px-2 rounded-xl cursor-pointer`}>{children}</button>
     </div>
   );
 }
 
+
 export default function App() {
+  const [view, setView] = useState(null);
+
   return (
-    <>
       <div className="container p-8 flex flex-col gap-8">
-        <Button>Click The Count</Button>
-        <Button>Track The Tasks</Button>
+        {view === null && (
+          <>
+          <Button view={view} onClick={() => setView('counter')}>Click The Count</Button>
+          <Button view={view} onClick={() => setView('todo')}>Track The Tasks</Button>
+          </>
+        )}
+        {view !== null && (
+          <>
+          <Button view={view} onClick={() => setView(null)}>Home</Button>
+          </>
+        )}
+
+        {view === 'counter' && <Counter />}
+        {view === 'todo' && <ToDoList />}
       </div>
-    </>
   );
 }
