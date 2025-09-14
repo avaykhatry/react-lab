0d7df143563fa134bdb2c8ed62de46d7477f780e
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-14 13:02:33 +0545

    chore: Implement  a simple runtime theming mecahnism using CSS variables

--- 01-mini-react-proj/src/ColorPicker.jsx
+++ 01-mini-react-proj/src/ColorPicker.jsx
@@ -1,32 +1,19 @@
-import { useState } from "react"
-import { Button } from "./App"
-
-
+import { Button } from "./App";
 
 export default function ColorPick() {
-    const [color, setColor] = useState(null);
+  function handleClick(clr) {
+    const currentClr =
+      document.documentElement.style.getPropertyValue("--bg-color");
+    currentClr === clr
+      ? document.documentElement.style.setProperty("--bg-color", "#242424")
+      : document.documentElement.style.setProperty("--bg-color", clr);
+  }
 
-    
-    const colors = {
-        red: 'bg-red-500',
-        blue: 'bg-blue-500',
-        pink: 'bg-pink-500',
-        green: 'bg-green-500',
-        amber: 'bg-amber-500',
-    }
-    
-    function handleClick(clr) {
-        setColor((color) => color === colors[clr] ? null : colors[clr]);
-    }
-
-    return(
-        <div className={`${color} flex justify-center gap-8 items-center`}>
-            <Button onClick={() => handleClick('red')}>Red</Button>
-            <Button onClick={() => handleClick('blue')}>Blue</Button>
-            <Button onClick={() => handleClick('green')}>Green</Button>
-            <Button onClick={() => handleClick('pink')}>Pink</Button>
-            <Button onClick={() => handleClick('amber')}>Amber</Button>
-            {console.log(color)}
-        </div>
-    )
+  return (
+    <div className={`flex justify-center gap-8 items-center`}>
+      <Button onClick={() => handleClick("red")}>Red</Button>
+      <Button onClick={() => handleClick("blue")}>Blue</Button>
+      <Button onClick={() => handleClick("green")}>Green</Button>
+    </div>
+  );
 }
