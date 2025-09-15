dabbb9e588ae14fe470e405df37b89143f4ce266
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-15 17:03:58 +0545

    chore: Generate new quote on every new click

--- 01-mini-react-proj/src/QuoteGen.jsx
+++ 01-mini-react-proj/src/QuoteGen.jsx
@@ -2,28 +2,38 @@ import { useEffect, useState } from "react";
 import { Button } from "./App";
 
 export default function Quote() {
-  const [data, setData] = useState(null);
+  const [quote, setQuote] = useState(null);
+  const [newq, setNewq] = useState(null);
 
   useEffect(() => {
-    fetch("https://dummyjson.com/quotes")
-      .then((response) => response.json())
-      .then((json) => (
-        setData(json)
-    ))
-      .catch((error) => console.error(error));
+    async function fetchApi() {
+      try {
+        const response = await fetch("https://dummyjson.com/quotes");
+        const data = await response.json();
+        setQuote(data);
+      } catch {
+        const error = console.error(error);
+      }
+    }
+    fetchApi();
   }, []);
 
-  function handleClick() {
-    return console.log(data);
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
   }
 
+  function handleClick(num) {
+    let position = getRandomInt(num);
+    let myQuote = quote.quotes[position].quote;
+    setNewq(myQuote);
+  }
+
+  
   return (
-    <div className="box-border">
-      <h1 className="text-6xl">Quote Generator</h1>
-      <Button onClick={() => handleClick} className={`my-8`}>
-        Get me quote
-      </Button>
-      {data}
-    </div>
+      <>
+      <Button onClick={() => handleClick(quote.quotes.length)}>Get a quote</Button>
+    {newq === null && 'loading...'}      
+<h1>{newq}</h1>
+    </>
   );
 }
