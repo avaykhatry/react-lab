69041105d5dea0a677af24646a59f270126bce27
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-16 22:31:48 +0545

    feat: Add input option to add name, email, tel

--- 02-cv-application/src/App.jsx
+++ 02-cv-application/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
 
+  function Input(props) {
+    return(
+      <>
+      <input id={props.id} type={props.type} />
+      <label for={props.for}>{props.name}</label>
+      </>
+    )
+  }
+
+  
 function App() {
   
 
   return (
     <>
       <h1>Build Your CV</h1>
+      <form>
+        <Input type='text' id="name" for="name" name="Name"/>
+        <Input type='email' id='email' for='email' name="Email"/>
+        <Input type="tel" id='phone' for='phone' name="Phone"/>
+      </form>
     </>
   )
 }
