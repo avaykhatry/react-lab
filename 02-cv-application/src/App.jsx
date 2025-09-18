1f9041e5e3abeedee3b827951a37d4540469263e
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-09-18 21:31:33 +0545

    chore: Update ui to show the cv and input fields on the single page

--- 02-cv-application/src/App.jsx
+++ 02-cv-application/src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import styles from "./App.module.css";
+import Cv from "./Cv";
 
 function Input({ type, id, label }) {
   return (
-    <div className={styles.nav}>
+    <div className={styles.hero}>
       <label htmlFor={id}>{label}</label>
       <input id={id} type={type} />
     </div>
@@ -13,7 +15,85 @@ function Button({ children }) {
   return <button>{children}</button>;
 }
 
+export function About() {
+  return (
+    <>
+      <form id="about" className={styles.about}>
+        <Input type="text" id="name" label="Full name" />
+        <Input type="email" id="email" label="Email" />
+        <Input type="number" id="phone" label="Phone" />
+        <Input type="text" id="location" label="Location" />
+      </form>
+    </>
+  );
+}
+
+export function ProfessionalSummary() {
+  return (
+    <>
+      <form id="prof-summary" className={styles.professionalSummary}>
+        <Input
+          type="text"
+          id="professional-summary"
+          label="Professional Summary"
+        />
+      </form>
+    </>
+  );
+}
+
+export function Experience() {
+  return (
+    <>
+      <form id="experience" className={styles.experience}>
+        <Input type="text" id="job-title" label="Job Title" />
+        <Input type="text" id="job-organization" label="Organization" />
+        <Input type="text" id="job-location" label="Location" />
+        <Input type="date" id="job-start-date" label="Start date" />
+        <Input type="date" id="job-end-date" label="End date" />
+        <Input type="text" id="job-description" label="Description" />
+        <Input type="checkbox" id="job-status" label="I currently work here" />
+      </form>
+    </>
+  );
+}
+
+export function Education() {
+  return (
+    <>
+      <form id="education" className={styles.education}>
+        <Input type="text" id="school" label="School" />
+        <Input type="text" id="degree" label="Degree" />
+        <Input type="text" id="location" label="Location" />
+        <Input type="date" id="start-date" label="Start Date" />
+        <Input type="date" id="grad-date" label="Graduation Date" />
+        <Input type="checkbox" id="edu-status" label="I currently study here" />
+      </form>
+    </>
+  );
+}
+
+export function Skills() {
+  return (
+    <>
+      <form id="skills" className={styles.skills}>
+        <Input type="text" id="skill-name" label="Skill name" />
+      </form>
+    </>
+  );
+}
+
 function App() {
+  const [view, setView] = useState(null);
+
+  const components = {
+    'about': <About />,
+    'professional-summary': <ProfessionalSummary />,
+    'experience': <Experience />,
+    'education': <Education />,
+    'skills': <Skills />
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -21,57 +101,33 @@ function App() {
         <button>Download</button>
         <button>Share</button>
       </div>
-      <div className={styles.main}>
+      
+        <div className={styles.nav}>
+          <div className={styles.navTitle}>
+            <a onClick={() => setView('about')} href="#about">About</a>
+          </div>
+          <div className={styles.navTitle}>
+            <a onClick={() => setView('professional-summary')} href="#prof-summary">Professional Summary</a>
+          </div>
+          <div className={styles.navTitle}>
+            <a onClick={() => setView('experience')} href="#experience">Experience</a>
+          </div>
+          <div className={styles.navTitle}>
+            <a onClick={() => setView('education')} href="#education">Education</a>
+          </div>
+          <div className={styles.navTitle}>
+            <a onClick={() => setView('skills')} href="#skills">Skills</a>
+          </div>
+        </div>
+        <div className={styles.main}>
         <div className={styles.left}>
-          <div className={styles.leftTitle}>
-            <a href="#about">About</a> 
-          </div>
-          <div className={styles.leftTitle}>
-            <a href="#prof-summary">Professional Summary</a>
-          </div>
-          <div className={styles.leftTitle}>
-            <a href="#experience">Experience</a>
-          </div>
-          <div className={styles.leftTitle}>
-            <a href="#education">Education</a>
-          </div>
-          <div className={styles.leftTitle}>
-            <a href="#skills">Skills</a>
-          </div>
+          {view === null && setView('about')}
+          {components[view]}
         </div>
         <div className={styles.right}>
-          <form id="about" className={styles.about}>
-            <Input type="text" id="name" label="Full name" />
-            <Input type="email" id="email" label="Email" />
-            <Input type="number" id="phone" label="Phone" />
-            <Input type="text" id="location" label="Location" />
-          </form>
+          <Cv />
         </div>
-        <div className={styles.forms}>
-          <form id="experience" className={styles.experience}>
-            <Input type="text" id="job-title" label="Job Title" />
-            <Input type="text" id="job-organization" label="Organization" />
-            <Input type="text" id="job-location" label="Location" />
-            <Input type="date" id="job-start-date" label="Start date" />
-            <Input type="date" id="job-end-date" label="End date" />
-            <Input type="text" id="job-description" label="Description" />
-            <Input type="checkbox" id="job-status" label="I currently work here" />
-          </form>
-          <form id="education" className={styles.education}>
-            <Input type="text" id="school" label="School" />
-            <Input type="text" id="degree" label="Degree" />
-            <Input type="text" id="location" label="Location" />
-            <Input type="date" id="start-date" label="Start Date" />
-            <Input type="date" id="grad-date" label="Graduation Date" />
-            <Input type="checkbox" id="edu-status" label="I currently study here" />
-          </form>
-          <form id="skills" className={styles.skills}>
-            <Input type="text" id="skill-name" label="Skill name" />
-          </form>
-          <form id="prof-summary" className={styles.professionalSummary}>
-            <Input type="text" id="professional-summary" label="Professional Summary" />
-          </form>
-        </div>
+
       </div>
     </div>
   );
