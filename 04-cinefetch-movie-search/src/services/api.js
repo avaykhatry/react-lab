e1d7b4e6603477abc44945e08b3c616ac58d825d
avay <130426359+avaykhatry@users.noreply.github.com>
2025-09-26 13:13:49 +0545

    feat: Call movie, tv shows discovering API

---
+++ 04-cinefetch-movie-search/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = 'ebbda0bc75b202eb11382cec4a9942d7';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
