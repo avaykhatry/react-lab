37fc0cedfd92327ac7820d2f8b04e1ea06d0610b
avay <130426359+avaykhatry@users.noreply.github.com>
2025-09-26 10:33:51 +0545

    feat: Add favorites page and set the route path

--- 04-cinefetch-movie-search/src/App.jsx
+++ 04-cinefetch-movie-search/src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
-import MovieCard from './components/MovieCard'
-
+import NavBar from './components/NavBar'
+import Favorites from './pages/Favorites'
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
-  return <div>
-      <MovieCard movie={{title: "Harry Potter", release_date: "2025"}}/>
-    <Home />
-  </div>
-  
+
+
+  return (
+    <div>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/favorites' element={<Favorites />}/>
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ 04-cinefetch-movie-search/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No Favorite Movies Here</h2>
+        <p>Add your favorite movies here, so you never lose them...</p>
+    </div>
+}
+
+export default Favorites
