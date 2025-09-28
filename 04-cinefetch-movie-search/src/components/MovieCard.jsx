30f67d83db1a42ab36ff6cfe3a8dbf2d00749abf
avay <130426359+avaykhatry@users.noreply.github.com>
2025-09-28 14:15:44 +0545

    fix: use corrected addToFavorites function from context

--- 04-cinefetch-movie-search/src/components/MovieCard.jsx
+++ 04-cinefetch-movie-search/src/components/MovieCard.jsx
@@ -1,17 +1,23 @@
+import { useMovieContext } from '../contexts/MovieContext'
 import '../css/MovieCard.css'
 
 function MovieCard({ movie }){
+    // console.log(useMovieContext());
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext();
+    const favorite = isFavorite(movie.id);
 
-    function onFavoriteClick(){
-        alert('clicked')
+    function onFavoriteClick(e){
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
     }
 
     return <div className="movie-card">
             <div className="movie-poster">
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie.title" />
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                 <div className="movie-overlay">
-                    <button className="favorite-btn" onClick={onFavoriteClick}>
-                        🩶
+                    <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                        ♥
                     </button>
                 </div>
             </div>
