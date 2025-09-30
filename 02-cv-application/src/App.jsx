a21478921722aadaec8cf1674d0234a373f7142b
avay <130426359+avaykhatry@users.noreply.github.com>
2025-09-30 22:55:46 +0545

    chore: Organize App and main files according to conventional rules

--- 02-cv-application/src/App.jsx
+++ 02-cv-application/src/App.jsx
@@ -1,109 +1,12 @@
-import { useState } from "react";
-import styles from "./App.module.css";
-import Cv from "./Cv";
-import { Experience } from "./feature/components/Experience";
-import { Education } from "./feature/components/Education";
-import { About } from "./feature/components/About";
-import { ProfessionalSummary } from "./feature/components/ProfessionalSummary";
-import { Button } from "./components/Input/Button";
-import { Input } from "./components/Input/Input";
-
-
-export function Skills({ setSkill }) {
-
-  function handleChange(e) {
-    setSkill(
-      e.target.value,
-    )
-  }
-  return (
-    <>
-      <form id="skills" className={styles.skills}>
-        <Input handleChange={handleChange} type="text" id="skill-name" label="Skill name" placeholder={'React, SQL, Figma'} />
-      </form>
-      <br />
-    </>
-  );
-}
+import About from "./feature/components/About";
+import Education from "./feature/components/Education";
+import Experience from "./feature/components/Experience";
+import Cv from "./feature/components/Cv";
 
 function App() {
-  const [view, setView] = useState("");
-  const [desc, setDesc] = useState("Directed the design of Money Movement SDK, collaborating with an agile cross-functional team, and managing the full design process from competitive research to mockup, and handoff.");
-  const [about, setAbout] = useState({
-    name: 'Harry Potter',
-    email: 'harry.potter@hogwarts.edu',
-    location: 'tree house',
-    phone: '0********'
-  });
-  const [summary, setSummary] = useState('A product designer with expertise leading end-to-end design processes from scratch, specializing in mobile and responsive web design, and thriving in cross-functional environment.');
-  const [experience, setExperience] = useState({
-    jobTitle: 'EXPERIENCE',
-    jobOrganization: 'Product Designer, Marqeta',
-    jobLocation: 'B2B Fintech',
-    jobStart: 'Jun 2024',
-    jobEnd: 'Present',
-    jobDescription: [],
-  })
-
-  const [education, setEducation] = useState({
-    school: 'Westcliff University',
-    degree: 'B.S., Information Technology',
-    schoolLocation: 'California, USA',
-    schoolStartDate: 'Apr 2022',
-    gradDate: 'Apr 2026',
-    gpa: 3.99,
-  })
-
-  const [skill, setSkill] = useState('React');
-
-
-
-  const components = {
-    'about': <About setAbout={setAbout} about={about} />,
-    'professional-summary': <ProfessionalSummary setSummary={setSummary} />,
-    'experience': <Experience desc={desc} setDesc={setDesc} experience={experience} setExperience={setExperience} />,
-    'education': <Education education={education} setEducation={setEducation} />,
-    'skills': <Skills setSkill={setSkill} />
-  }
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.logo}>Build Your CV</div>
-        <button>Download</button>
-        <button>Share</button>
-      </div>
-      
-        <div className={styles.nav}>
-          <div className={styles.navTitle}>
-            <a onClick={() => setView('about')} href="#about">About</a>
-          </div>
-          <div className={styles.navTitle}>
-            <a onClick={() => setView('professional-summary')} href="#prof-summary">Professional Summary</a>
-          </div>
-          <div className={styles.navTitle}>
-            <a onClick={() => setView('experience')} href="#experience">Experience</a>
-          </div>
-          <div className={styles.navTitle}>
-            <a onClick={() => setView('education')} href="#education">Education</a>
-          </div>
-          <div className={styles.navTitle}>
-            <a onClick={() => setView('skills')} href="#skills">Skills</a>
-          </div>
-        </div>
-        <div className={styles.main}>
-        <div className={styles.left}>
-          {view === "" && setView('about')}
-          {components[view]}
-        </div>
-        <div className={styles.right}>
-          <Cv name={about.name} email={about.email} location={about.location} phone={about.phone} summary={summary} skill={skill} experience={experience} education={education} setExperience={setExperience} />
-          </div>
-        </div>
-
-      </div>
-    </div>
-  );
+  return <div className="main-content">
+    <Cv />
+  </div>
 }
 
 export default App;
--- 02-cv-application/src/main.jsx
+++ 02-cv-application/src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import './styles/index.css'
 import App from './App.jsx'
-import Cv from './Cv.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
