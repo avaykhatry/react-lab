36301c1855967a35f6cd941d0dcb88359fa8971a
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-05 20:58:38 +0545

    chore: move shared components to different folder

--- 02-cv-application/src/components/Input/Button.jsx
+++
@@ -1,5 +0,0 @@
-import styles from '../../styles/App.module.css';
-
-export function Button({ handleClick, children }) {
-  return <button onClick={handleClick} className={styles.descBtn}>{children}</button>;
-}
