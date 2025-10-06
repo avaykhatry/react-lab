4bc6146c5ff79b14f70f79b6d476d28d2a0a04ed
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-06 13:19:27 +0545

    feat: sync about section updates across input and CV display components

--- 02-cv-application/src/feature/components/About.jsx
+++ 02-cv-application/src/feature/components/About.jsx
@@ -1,18 +1,45 @@
-import { useState } from "react";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import styles from "../../styles/App.module.css";
 
-function About() {
-  const [value, setValue] = useState("");
+function About({ setCvDetails, value, setValue }) {
 
+  function handleNameChange(e) {
+    setValue(e.target.value)
+    setCvDetails(cvDetails => ({
+      ...cvDetails,
+      about: {
+        ...cvDetails.about,
+        name : e.target.value,
+      }
+    }))
+  }
+
+  function handleContactsChange(e) {
+    setValue(e.target.value)
+
+    setCvDetails(cvDetails => ({
+      ...cvDetails,
+      about : {
+        ...cvDetails.about,
+        contacts : cvDetails.about.contacts.map(
+          contact => (
+            contact.type === e.target.id ?
+            { ...contact, value } :
+            contact
+          )
+        )
+      }
+    }))
+  }
   return (
     <div>
       <h2 className={styles.title}>About</h2>
       <div className={styles.section}>
-      <Input value={value} type="text" id='fullName' label="Full Name" handleChange={e => setValue(e.target.value)} placeholder="John Allen"/>
-      <Input value={value} type="text" id="email" label="Email" handleChange={e => setValue(e.target.value)} placeholder='john.allen@gmail.com'/>
-      <Input value={value} type="number" id="phone" label="Phone" handleChange={e => setValue(e.target.value)} placeholder='977989782939*'/>
+      <Input value={value} type="text" id='fullName' label="Full Name" handleChange={handleNameChange} placeholder="John Allen"/>
+      <Input value={value} type="number" id="tel:" label="Phone" handleChange={handleContactsChange} placeholder='977989782939*'/>
+      <Input value={value} type="text" id="mailto:" label="Email" handleChange={handleContactsChange} placeholder='john.allen@gmail.com'/>
+      <Input value={value} type="text" id="text" label="Location" handleChange={handleContactsChange} placeholder='Austin, TX'/>
     </div>
   </div>
 )}
