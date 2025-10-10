f449931f96caaa0629339162b9350b50fb88eade
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-10 22:36:50 +0545

    feat: wrap App component with context provider

--- 02-cv-application/src/main.jsx
+++ 02-cv-application/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
+import { CvProvider } from './contexts/CvProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+      <CvProvider>
+        <App />
+      </CvProvider>
   </StrictMode>,
 )
