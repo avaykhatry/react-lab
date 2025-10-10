51c1062bd1a96e8a5cb6a624a0acc2365b3a0976
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-10 22:44:10 +0545

    feat: configure BrowserRouter for global routing access
    wrap whole app so that all components can access the routing configuration

--- 02-cv-application/src/main.jsx
+++ 02-cv-application/src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 import { CvProvider } from './contexts/CvProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
       <CvProvider>
         <App />
       </CvProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
