eba238bbef7442bee445a4948e90cce64c24eaff
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-13 23:34:38 +0545

    chore: hide header and footer while converting to pdf

--- 02-cv-application/src/layout/Footer.jsx
+++ 02-cv-application/src/layout/Footer.jsx
@@ -2,8 +2,8 @@ import styles from '../styles/App.module.css'
 
 export default function Footer() {
     return (
-        <>
+        <div className='footer'>
          <p className={styles.logo}>Build by avk</p>
-        </>
+        </div>
     )
 }
--- 02-cv-application/src/layout/Header.jsx
+++ 02-cv-application/src/layout/Header.jsx
@@ -2,8 +2,8 @@ import styles from '../styles/App.module.css'
 
 export default function Header() {
     return (
-        <>
+        <div className='header'>
         <h1 className={styles.logo}>CV Builder</h1>
-        </>
+        </div>
     )
 }
