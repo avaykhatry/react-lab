494da1a3ef31430c88c2e5e9bb559a5ca076acbd
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-16 23:04:05 +0545

    style: style input component

--- 02-cv-application/src/components/Input.jsx
+++ 02-cv-application/src/components/Input.jsx
@@ -1,11 +1,11 @@
-import styles from '../styles/App.module.css'
+import styles from './Input.module.css'
 
-export function Input({ type, id, label, handleChange, placeholder }) {
+export function Input({ type, id, label, handleChange, placeholder, style }) {
   
   return (
-    <div className={styles.hero}>
+    <div className={styles.inputComponent}>
       <label htmlFor={id}>{label}</label>
-      <input placeholder={placeholder} onChange={handleChange} id={id} type={type} />
+      <input style={style} className={styles.mainInput} placeholder={placeholder} onChange={handleChange} id={id} type={type} />
     </div>
   );
 }
