87a0f4813874d9200353b0521d81491385c43a1f
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-20 13:20:43 +0545

    chore: import feature components in App.jsx

--- 01-mini-react-proj/src/App.jsx
+++ 01-mini-react-proj/src/App.jsx
@@ -1,55 +1,54 @@
-import { useState } from "react";
-import Counter from "./Counter";
-import ToDoList from "./ToDoList";
-import ColorPick from "./ColorPicker";
-import Quote from "./QuoteGen";
-import ToggleTheme from "./ThemeToggle";
-
-export function Button({ onClick, children, className }) {
-  return (
-    <div>
-      <button onClick={onClick} className={`border px-2 rounded-xl cursor-pointer ${className}`}>{children}</button>
-    </div>
-  );
-}
-// alt to above function
-// export function Button({ children, ...props }) {
-//   return (
-//       <button {...props} className={` border px-2 rounded-xl cursor-pointer`}>{children}</button>
-//   );
-// }
+import Counter from "./pages/Counter";
+import ToDoList from "./pages/ToDoList";
+import ColorPick from "./pages/ColorPicker";
+import Quote from "./pages/QuoteGen";
+import ToggleTheme from "./pages/ThemeToggle";
+import { Route, Routes } from "react-router-dom";
+import { NavBar } from "./layout/NavBar";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 
 export default function App() {
-  const [view, setView] = useState(null);
 
   return (
-      <div className="container p-8 flex flex-col gap-8">
-        {view === null && (
-          <>
-          <Button view={view} onClick={() => setView('counter')}>Click The Count</Button>
-          <Button view={view} onClick={() => setView('todo')}>Track The Tasks</Button>
-          <Button view={view} onClick={() => setView('colorpicker')}>Color Picker</Button>
-          <Button view={view} onClick={() => setView('quotegen')}>Quote Generator</Button>
-          <Button view={view} onClick={() => setView('theme')}>
-            Toggle Theme
-          </Button>
-          </>
+      // <div className="container p-8 flex flex-col gap-8">
+      //   {view === null && (
+      //     <>
+      //     <Button view={view} onClick={() => setView('counter')}>Click The Count</Button>
+      //     <Button view={view} onClick={() => setView('todo')}>Track The Tasks</Button>
+      //     <Button view={view} onClick={() => setView('colorpicker')}>Color Picker</Button>
+      //     <Button view={view} onClick={() => setView('quotegen')}>Quote Generator</Button>
+      //     <Button view={view} onClick={() => setView('theme')}>
+      //       Toggle Theme
+      //     </Button>
+      //     </>
 
-        )}
-        {view !== null && (
-          <>
-          <Button view={view} onClick={() => setView(null)}>Home</Button>
-          </>
-        )}
+      //   )}
+      //   {view !== null && (
+      //     <>
+      //     <Button view={view} onClick={() => setView(null)}>Home</Button>
+      //     </>
+      //   )}
 
-        {
-          view === 'counter' ? <Counter /> 
-          : view === 'todo' ? <ToDoList /> 
-          : view === 'colorpicker' ? <ColorPick /> 
-          : view === 'quotegen' ? <Quote /> 
-          : view === 'theme' ? <ToggleTheme />
-          : null 
-        }
+      //   {
+      //     view === 'counter' ? <Counter /> 
+      //     : view === 'todo' ? <ToDoList /> 
+      //     : view === 'colorpicker' ? <ColorPick /> 
+      //     : view === 'quotegen' ? <Quote /> 
+      //     : view === 'theme' ? <ToggleTheme />
+      //     : null 
+      //   }
+      // </div>
+      <div>
+        <Header />
+        <NavBar />
+        <Routes>
+          <Route path='/counter' element={<Counter />} />
+          <Route path="/colorpicker" element={<ColorPick />} />
+          <Route path="/todo" element={<ToDoList />} />
+          <Route path="/quotegen" element={<Quote />} />
+        </Routes>
+        <Footer />
       </div>
   );
 }
