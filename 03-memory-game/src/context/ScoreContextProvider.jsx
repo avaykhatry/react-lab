7da801020fa9cf30f229639805e118ec1207b0d1
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-22 23:52:19 +0545

    feat: add ScoreContextProvider with score, bestScore, and mode state management

---
+++ 03-memory-game/src/context/ScoreContextProvider.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { ScoreContext } from "./ScoreContext";
+
+export function ScoreContextProvider({ children }){
+    const [score, setScore] = useState(0);
+    const [bestScore, setBestScore] = useState(localStorage.getItem('bestScore'));
+    const [selectedMode, setSelectedMode] = useState('easy');
+
+    return (
+        <ScoreContext.Provider value={{ score, setScore, bestScore, setBestScore, selectedMode, setSelectedMode }}>
+            {children}
+        </ScoreContext.Provider>
+    )
+}
