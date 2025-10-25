388b69e8f3c520ace19ce81911fe9ef80e167531
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-25 21:56:16 +0545

    feat: wrap App component with ScoreContextProvider

--- 03-memory-game/src/main.jsx
+++ 03-memory-game/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ScoreContextProvider } from './context/ScoreContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ScoreContextProvider>
+      <App />
+    </ScoreContextProvider>
   </StrictMode>,
 )
