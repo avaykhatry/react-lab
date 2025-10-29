2eb4f051df65e99b0fe734c424bcb9e8ecbb56ed
avay <130426359+avaykhatry@users.noreply.github.com>
2025-10-29 22:11:35 +0545

    feat: add object for easy path refactoring

--- 05-mini-react-router-setup/src/route/routes.jsx
+++ 05-mini-react-router-setup/src/route/routes.jsx
@@ -4,23 +4,31 @@ import Applied from "../pages/Applied";
 import Interview from "../pages/Interview";
 import Offer from "../pages/Offer";
 
+export const ROUTES = {
+    ROOT: '/',
+    DASHBOARD: '/dashboard',
+    APPLIED: '/applied',
+    INTERVIEW: '/interview',
+    OFFER: '/offer',
+};
+
 const routes = [
     {
-        path: '/',
-        element: <Navigate to='/applied' />,
+        path: ROUTES.ROOT,
+        element: <Navigate to={ROUTES.APPLIED} />,
     },
     // do conditional navigation like this
     // { path: '/', element: isLoggedIn ? <Dashboard /> : <Navigate to='/login' />}
     {
-        path:'/applied',
+        path: ROUTES.APPLIED,
         element: <Applied />,
     },
     {
-        path: '/interview',
+        path: ROUTES.INTERVIEW,
         element: <Interview />,
     },
     {
-        path: '/offer',
+        path: ROUTES.OFFER,
         element: <Offer />,
     }
 ]
